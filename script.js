86e21e1372d7fc8ed4a2afb7b35ce8f397c5a508
rit229 <malikritu845@gmail.com>
2025-10-27 10:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function functionName() {
+            alert(functionName.name); // This will alert the function's name
+        }
+
+        functionName(); // Call the function
+    
